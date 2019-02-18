e092c9e29a9763a538dcde572a2b4c4417d89d16
Michael Utz <michael@theutz.com>
2019-02-18 20:35:41 +0200

    chore: apply prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return <h1>My 90 Ride</h1>;
+    return <h1>My 90 Ride</h1>
   }
 }
 
-export default App;
+export default App
